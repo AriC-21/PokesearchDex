ce9a4e8a6496c510556b4fac258425594c691c57
AriC-21 <92972785+AriC-21@users.noreply.github.com>
2022-02-22 01:27:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,11 @@ const displayPokemon = (pokemon) => {
 };
 
 fetchPokemon() ;
+
 //This search filter is not working
+
 const search_term = document.getElementById('search-input');
 const search_btn = document.getElementById('search-button');
 search_btn.addEventListener('click', () => getPokemonData(search_term.value));
+displayPokemonData(search_term.value)
 
