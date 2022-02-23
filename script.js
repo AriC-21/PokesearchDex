180692cc52a1faaeb5e5748bd23bb9bf0f13fa56
AriC-21 <92972785+AriC-21@users.noreply.github.com>
2022-02-23 17:13:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
 const pokedex = document.getElementById('pokedex');
-
+let data = [];
 const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 50; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_shiny'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
-        }));
-        displayPokemon(pokemon);
-    });
+  const promises = [];
+  for (let i = 1; i <= 50; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
+      name: result.name,
+      image: result.sprites['front_shiny'],
+      type: result.types.map((type) => type.type.name).join(', '),
+      id: result.id,
+    }));
+    data = pokemon;
+    displayPokemon(pokemon);
+  });
 };
 
-
 const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon
-        .map(
-            (poke) => `
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (poke) => `
         <li class="card">
             <img class="card-image" src="${poke.image}"/>
             <h2 class="card-title">${poke.id}. ${poke.name}</h2>
             <p class="card-subtitle">Type: ${poke.type}</p>
         </li>
     `
-        )
-        .join('');
-    pokedex.innerHTML = pokemonHTMLString;
+    )
+    .join('');
+  pokedex.innerHTML = pokemonHTMLString;
 };
 
-fetchPokemon() ;
+const getPokemonData = (event, searchText) => {
+  event.preventDefault();
+  const filterData = data.filter(
+    (item) => item.name.toUpperCase() == searchText.toUpperCase()
+  );
+  const pokemonHTMLString = filterData
+    .map(
+      (poke) => `
+            <li class="card">
+                <img class="card-image" src="${poke.image}"/>
+                <h2 class="card-title">${poke.id}. ${poke.name}</h2>
+                <p class="card-subtitle">Type: ${poke.type}</p>
+            </li>
+        `
+    )
+    .join('');
+  pokedex.innerHTML = pokemonHTMLString;
+};
 
 const search_term = document.getElementById('search-input');
 const search_btn = document.getElementById('search-button');
@@ -42,4 +59,4 @@ search_btn.addEventListener('click', (e) =>
   getPokemonData(e, search_term.value)
 );
 
-fetchPokemon() ;
+fetchPokemon();
